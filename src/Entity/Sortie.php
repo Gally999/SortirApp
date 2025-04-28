3139e386f41de133dd059b59c0f70c35ddf003d2
Gally <gally999@free.fr>
2025-04-28 23:11:46 +0200

    add fixtures for entities

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -2,13 +2,13 @@
 
 namespace App\Entity;
 
+use App\Enum\EtatEnum;
 use App\Repository\SortieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Validator\Constraints as Assert;
 
 #[UniqueEntity(fields: ['nom', 'dateHeureDebut', 'lieu'], message: 'Cette sortie existe déjà.')]
 #[ORM\Entity(repositoryClass: SortieRepository::class)]
