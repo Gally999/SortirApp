b9c8109a74781ad46fe2df203f82bbabecc46354
Goadney <g.nacher@hotmail.com>
2025-04-30 11:43:22 +0200

    feat(participant): profile picture

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -6,9 +6,10 @@ use App\Entity\Participant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
 
 class ParticipantType extends AbstractType
 {
@@ -39,7 +40,13 @@ class ParticipantType extends AbstractType
                 'required' => false,
                 'label' => 'Numéro de téléphone',
                 'attr' => ['class' => 'form-control', 'placeholder' => 'Entrez votre numéro de téléphone']
-            ]);
+            ])
+            ->add('profilePicture', FileType::class, [
+                'label' => 'Photo de profil',
+                'required' => false,
+                'mapped' => false,
+
+            ]);;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
