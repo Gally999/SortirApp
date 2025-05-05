6a0e6cac0496e4df7cff077ef4be2d5e0d8cb023
Greg <g.nacher@hotmail.com>
2025-05-05 18:04:06 +0200

    fix datatable + commentaire

--- src/Controller/Admin/ParticipantController.php
+++ src/Controller/Admin/ParticipantController.php
@@ -15,6 +15,70 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/admin/participants', name: 'admin_participants')]
 class ParticipantController extends AbstractController
 {
+
+    /**
+     * Handles the modification of a participant's details.
+     *
+     * This method allows an admin to modify the details of a participant
+     * identified by their pseudo. It retrieves the participant, checks for 
+     * various authorization conditions, and handles the form submission 
+     * for updating the participant's information.
+     *
+     * @param string $pseudo The pseudo of the participant to modify.
+     * @param ParticipantRepository $participantsRepo The repository to find participants.
+     * @param Request $request The HTTP request object.
+     * @param EntityManagerInterface $entityManager The entity manager to handle database operations.
+     *
+     * @return Response The HTTP response, either rendering the modification form or redirecting.
+     */
+    #[Route('/modify/{pseudo}', name: '_modify', methods:['GET', 'POST'])]
+    public function modifyParticipant($pseudo,ParticipantRepository $participantsRepo, EntityManagerInterface $entityManager, Request $request):Response 
+    {
+        //récupération du participant avec le repository et une fonction custom
+        $participant = $participantsRepo->findParticipantByPseudo($pseudo);
+
+        // S'il n'éxiste pas, on redirige vers la liste des participants avec une erreur en notification flash
+        if(!$participant){
+            $this->addFlash('error', 'Participant introuvable');
+            return $this->redirectToRoute('admin_participants_list');
+        }
+        // Si on est pas l'auteur du participant, on redirige vers la liste des participants avec une erreur en notification flash
+        elseif($this->getUser()->getId() == $participant->getId()){
+            return $this->redirectToRoute('app_profile');
+        }
+        // Si on n'est pas admin, on redirige vers la liste des participants avec une erreur en notification flash
+        elseif($this->getUser()->isAdministrateur() == false){
+            $this->addFlash('error', 'Vous devez etre admin pour modifier un participant');
+            return $this->redirectToRoute('app_home');
+        }
+        // Si le participant est admin, on redirige vers la liste des participants avec une erreur en notification flash
+        elseif($participant->isAdministrateur() == true){
+            $this->addFlash('error', 'Vous ne pouvez pas modifier un administrateur');
+            return $this->redirectToRoute('admin_participants_list');
+        }
+        // Création du form grace au FormType et on y passe le participant pour hydrater le formulaire
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // mise en file du participant
+            $entityManager->persist($participant);
+            // pousser dans la base de données
+            $entityManager->flush();
+            $this->addFlash('success', 'Participant modifié avec succès');
+            return $this->redirectToRoute('admin_participants_list');
+        }
+
+        return $this->render(
+            'admin/Participant/modifyParticipant.html.twig',
+            [
+                'form' => $form->createView(),
+                'participant' => $participant,
+            ]
+        );
+    }
+
+    
     #[Route('/', name: '_list', methods:['GET'])]
     public function listParticipant(ParticipantRepository $participantsRepo, CampusRepository $campusRepo): Response
     {
@@ -146,42 +210,6 @@ class ParticipantController extends AbstractController
         return $this->redirectToRoute('admin_participants_list');
     }
 
-    #[Route('/modify/{pseudo}', name: '_modify', methods:['GET', 'POST'])]
-    public function modifyParticipant($pseudo,ParticipantRepository $participantsRepo, Request $request, EntityManagerInterface $entityManager):Response 
-    {
-        $participant = $participantsRepo->findParticipantByPseudo($pseudo);
-        if(!$participant){
-            $this->addFlash('error', 'Participant introuvable');
-            return $this->redirectToRoute('admin_participants_list');
-        }
-        elseif($this->getUser()->getId() == $participant->getId()){
-            return $this->redirectToRoute('app_profile');
-        }
-        elseif($this->getUser()->isAdministrateur() == false){
-            $this->addFlash('error', 'Vous devez etre admin pour modifier un participant');
-            return $this->redirectToRoute('app_home');
-        }elseif($participant->isAdministrateur() == true){
-            $this->addFlash('error', 'Vous ne pouvez pas modifier un administrateur');
-            return $this->redirectToRoute('admin_participants_list');
-        }
-
-        $form = $this->createForm(ParticipantType::class, $participant);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($participant);
-            $entityManager->flush();
-            $this->addFlash('success', 'Participant modifié avec succès');
-            return $this->redirectToRoute('admin_participants_list');
-        }
-
-        return $this->render(
-            'admin/Participant/modifyParticipant.html.twig',
-            [
-                'form' => $form->createView(),
-                'participant' => $participant,
-            ]
-        );
-    }
+    
 
 }
